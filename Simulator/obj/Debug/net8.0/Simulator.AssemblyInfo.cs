7d7bc5fd95bfa7a2e4095d0819e4a8b133d0fe8d
Majki03 <majki03@int.pl>
2024-12-16 17:37:13 +0100

    Mapy

--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162676a02168b048f35a1700b86d6c1d1e5fc47f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74c8fbb930644eee9633944a5c2a0da44a6f7f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
